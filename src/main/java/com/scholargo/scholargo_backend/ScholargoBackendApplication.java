052aa992119f6ec5ef964971501f335114061aed
Katt <kthutsana7@gmail.com>
2026-07-11 23:37:20 +0200

    initializing backend repo

---
+++ src/main/java/com/scholargo/scholargo_backend/ScholargoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scholargo.scholargo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholargoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholargoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scholargo/scholargo_backend/ScholargoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholargo.scholargo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholargoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
